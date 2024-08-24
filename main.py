07a29e78fa5a8dac081ea62c661c2a56f8a567f4
Des Kitten <denis.schizarium@gmail.com>
2024-08-25 02:44:06 +0300

    refactored

--- main.py
+++ main.py
@@ -7,30 +7,6 @@ from fpdf import FPDF
 from fontTools import ttLib
 import numpy as np
 
-fontPath = "better-vcr-6.1.ttf"
-fontFamilyName = "RogueLike_RPG_MultyLang"
-
-from_font = ttLib.TTFont(fontPath)
-
-# total glyphs in font
-total = from_font['maxp'].numGlyphs
-print(f'There are {total} glyphs in the font')
-
-# extract cmap tables
-cm = from_font['cmap'].__dict__['tables']
-
-# quantity of tables
-qnty_tables = len(cm)
-print(f'the number of cmap table in this font is {qnty_tables}')
-
-# extract dicts of glyphs in different formats from cmap table
-all_tabs_expanded = []
-for i in range(0, qnty_tables):
-    all_tabs_expanded.append(cm[i].__dict__)
-
-# choose most valuable
-valuable_tables = [all_tabs_expanded[0], all_tabs_expanded[3]]
-
 
 def tab_to_lists(table_dict: dict) -> list:
     """ Return list of lists
@@ -40,7 +16,7 @@ def tab_to_lists(table_dict: dict) -> list:
     `ch_gl` is the render of the glyph/symbol, how it looks on screen,
     `name_gl` is the given name of the glyph if it has.
 
-    :return list[char, hex, dec, name] - order can be changed
+    :return list[order_number, char, hex, dec, name] - order can be changed
     """
     dex_gl, hex_gl, ch_gl, name_gl = ["DEX"], ["HEX"], ["CHAR"], ["NAME"]
     counter, counts = 1, ["№"]
@@ -65,7 +41,7 @@ def tab_to_lists(table_dict: dict) -> list:
     return [counts, ch_gl, hex_gl, dex_gl, name_gl]
 
 
-def create_pdf(table, output_file: str) -> None:
+def create_pdf(table, output_file: str, font_family_name: str, font_path: str) -> None:
     """Set up the pdf page, create a tables cells,
     and create a pdf file in current folder """
 
@@ -73,8 +49,8 @@ def create_pdf(table, output_file: str) -> None:
     pdf = FPDF(orientation="P", unit="pt", format="A4")
     pdf.font_size = font_size
     pdf.add_page()
-    pdf.add_font(family=fontFamilyName, fname=fontPath, uni=True)
-    pdf.set_font(family=fontFamilyName, size=font_size)
+    pdf.add_font(family=font_family_name, fname=font_path, uni=True)
+    pdf.set_font(family=font_family_name, size=font_size)
     col_width = pdf.w / 6
     row_height = pdf.font_size
     spacing = 2
@@ -86,13 +62,48 @@ def create_pdf(table, output_file: str) -> None:
     pdf.output(output_file)
 
 
-# convert cmap tables into list of values for each table
-tab_list = []
-for tab in valuable_tables:
-    tab_list.append(tab_to_lists(table_dict=tab))
+def main() -> None:
+    font_path = "better-vcr-6.1.ttf"
+    font_family_name = "RogueLike_RPG_MultyLang"
 
-# create a separate pdf file for each cmap table
-for index, in_row_tab in enumerate(tab_list):
-    # rows to columns
-    in_col_table = np.transpose(in_row_tab)
-    create_pdf(table=in_col_table, output_file=f'{fontFamilyName}_v{index}.pdf')
+    from_font = ttLib.TTFont(font_path)
+
+    # total glyphs in font
+    total = from_font['maxp'].numGlyphs
+    print(f'There are {total} glyphs in the font')
+
+    # extract cmap tables
+    cm = from_font['cmap'].__dict__['tables']
+
+    # quantity of tables
+    qnty_tables = len(cm)
+    print(f'the number of cmap table in this font is {qnty_tables}')
+
+    # extract dicts of glyphs in different formats from cmap table
+    all_tabs_expanded = []
+    for i in range(0, qnty_tables):
+        all_tabs_expanded.append(cm[i].__dict__)
+
+    # choose most valuable (chosen by testing)
+    valuable_tables = [all_tabs_expanded[0], all_tabs_expanded[3]]
+
+    # convert cmap tables into list of values for each table
+    tab_list = []
+    for tab in valuable_tables:
+        tab_list.append(tab_to_lists(table_dict=tab))
+
+    # create a separate pdf file for each cmap table
+    for index, in_row_tab in enumerate(tab_list):
+        # rows to columns
+        in_col_table = np.transpose(in_row_tab)
+
+        create_pdf(
+            table=in_col_table,
+            output_file=f'{font_family_name}_v{index}.pdf',
+            font_family_name=font_family_name,
+            font_path=font_path,
+        )
+
+
+if __name__ == "__main__":
+    main()
